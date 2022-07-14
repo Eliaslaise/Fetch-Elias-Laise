8d5637cfc2949ea0ae7e34e0a2944e58427c7fbc
EliasLaise <elias_laise@hotmail.com>
2022-07-13 21:55:45 -0300

    subida al servidor

---
+++ js/variables.js
@@ -0,0 +1,11 @@
+Swal.fire({
+    title:"bienvenido",
+    text:"Espero te guste mi Pág, en este caso, el uso de FETCH"
+});
+
+const contenidoDOM = document.querySelector("#contenido")
+const cargandoDOM = document.querySelector("#cargando")
+
+const URL = `js/trailerflix.json`
+//const URL = 'http://localhost:3050/trailerflix/'
+//const URL = `https://api.algunservidorremoto.com.au/pelis.json`
